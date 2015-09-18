8f0b8df958f239d94e710688923b84778c54e0fa
John Master <john.s.master@gmail.com>
2015-09-17 21:00:32 -0400

    serve correct assets

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 // =============================================================================
 
 // call the packages we need
-var express     = require('express');
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan')
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 // Set environment (not totally sure where the best place to put this is)
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var app         = express();
-var bodyParser  = require('body-parser');
-var mongoose    = require('mongoose');
+var app = express();
+
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
 var db          = require('./config/db');
 // call Models
 var Merchant    = require('./server/models/merchant');
@@ -16,13 +22,21 @@ var Product     = require('./server/models/product');
 
 mongoose.connect(db.url);
 
+// CONFIGURATION
+// =============================================================================
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));   //set static asset directory
 
 //use bodyParser to get Post data
 app.use(bodyParser.urlencoded({extended: true}));   //get params from url (I think)
 app.use(bodyParser.json());                       //parse as json
-app.use(express.static(__dirname + '/public'));   //set static asset directory
 
 var port = process.env.PORT || 5555;              //set port
 
