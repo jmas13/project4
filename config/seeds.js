9480a94c53afa53bd81b1235fd103e98a168ad68
John Master <john.s.master@gmail.com>
2015-09-17 23:44:47 -0400

    change seeds to work on heroku

--- config/seeds.js
+++ config/seeds.js
@@ -6,7 +6,13 @@ var Merchant    = require('../server/models/merchant');
 var Product     = require('../server/models/product');
 
 // TODO: Drop database or empty collections before seeding
-mongoose.connect('mongodb://localhost/mrkt');
+// TODO: Dry up below conditional (repeated in server.js)
+if (process.env.NODE_ENV == 'development') {
+  mongoose.connect('mongodb://localhost/mrkt');
+}
+else {
+  mongoose.connect('mongodb://'+ process.env.MONGOLAB_USER +':'+ process.env.MONGOLAB_PASS +'@ds027483.mongolab.com:27483/mrkt')
+}
 var db = mongoose.connection;
 // TODO: this is from mongoose quickstart -- look into exactly what it does (the console.error bit)
 db.on('error', console.error.bind(console, 'connection error:'));
